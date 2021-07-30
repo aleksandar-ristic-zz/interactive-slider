538b6b1df614c8f76f7f1c671c9f4712b81c6ba2
aleksandar-ristic <aristic665@gmail.com>
2021-07-30 18:14:56 +0200

    added responive

---
+++ dist/script.js
@@ -0,0 +1,16 @@
+// Selectors
+const inputRange = document.querySelector('#range')
+const inputCheck = document.querySelector('#check')
+const inputValue = document.querySelector('.amount')
+const inputFillValue = document.querySelector('.active-slide')
+
+// Vars
+let rangeValue = inputRange.value
+let check = inputCheck.checked
+let fillValue = 53.34
+inputValue = rangeValue
+inputFillValue.style.width = `${fillValue}%`
+
+// Event Listeners
+
+// Functions
